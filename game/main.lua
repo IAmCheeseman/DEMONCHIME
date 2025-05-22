6f02021edc8ee502936a1bfca7d33101431164c1
Xander Swan <xanderswan32@gmail.com>
2025-05-21 21:16:12 -0400

    split the "game" up into more than 1 file

--- game/main.lua
+++ game/main.lua
@@ -4,10 +4,7 @@ elseif core.os == "linux" then
   package.path = package.path .. ";./?/init.lua"
 end
 
-core.set_tick_rate(30)
-
-local tex = core.load_tex("res/textures/bricks.png")
-tex:set_filter(core.tex_filter.NEAREST, core.tex_filter.NEAREST)
+require("systems.draw_meshes")
 
 local fmt = core.create_vert_fmt()
 fmt:add_attr("float", 3) -- position
@@ -15,73 +12,8 @@ fmt:add_attr("float", 3) -- normal
 fmt:add_attr("float", 2) -- uv
 fmt:add_attr("float", 4) -- color
 
-local shader = core.load_shader("res/vdefault.glsl", "res/fdefault.glsl")
-
 local cube = core.create_mesh(fmt)
-local vp = {
-  {0.5, 0.5, -0.5},
-  {0.5, -0.5, -0.5},
-  {0.5, 0.5, 0.5},
-  {0.5, -0.5, 0.5},
-  {-0.5, 0.5, -0.5},
-  {-0.5, -0.5, -0.5},
-  {-0.5, 0.5, 0.5},
-  {-0.5, -0.5, 0.5},
-}
-
--- 1 2
--- |/|
--- 3 4
-
--- 1 2 3  1 3 4
-cube:set_vertices({
-  {vp[7][1], vp[7][2], vp[7][3],  0, 1, 0,  1, 1,  1, 1, 1, 1},
-  {vp[5][1], vp[5][2], vp[5][3],  0, 1, 0,  1, 0,  1, 1, 1, 1},
-  {vp[1][1], vp[1][2], vp[1][3],  0, 1, 0,  0, 0,  1, 1, 1, 1}, -- orange OK
-  {vp[3][1], vp[3][2], vp[3][3],  0, 1, 0,  0, 1,  1, 1, 1, 1},
-  {vp[7][1], vp[7][2], vp[7][3],  0, 1, 0,  1, 1,  1, 1, 1, 1},
-  {vp[1][1], vp[1][2], vp[1][3],  0, 1, 0,  0, 0,  1, 1, 1, 1},
-
-  -- 4 3 7 8
-  {vp[7][1], vp[7][2], vp[7][3],  0, 0, 1,  0, 1,  1, 1, 1, 1}, -- red
-  {vp[3][1], vp[3][2], vp[3][3],  0, 0, 1,  1, 1,  1, 1, 1, 1}, -- 1
-  {vp[4][1], vp[4][2], vp[4][3],  0, 0, 1,  1, 0,  1, 1, 1, 1}, -- 2
-  {vp[8][1], vp[8][2], vp[8][3],  0, 0, 1,  0, 0,  1, 1, 1, 1}, -- 3
-  {vp[7][1], vp[7][2], vp[7][3],  0, 0, 1,  0, 1,  1, 1, 1, 1}, -- 4
-  {vp[4][1], vp[4][2], vp[4][3],  0, 0, 1,  1, 0,  1, 1, 1, 1}, -- 5
-
-  -- 8 7 5 6
-  {vp[5][1], vp[5][2], vp[5][3],  -1, 0, 0,  0, 1,  1, 1, 1, 1}, -- green
-  {vp[7][1], vp[7][2], vp[7][3],  -1, 0, 0,  1, 1,  1, 1, 1, 1},
-  {vp[8][1], vp[8][2], vp[8][3],  -1, 0, 0,  1, 0,  1, 1, 1, 1},
-  {vp[6][1], vp[6][2], vp[6][3],  -1, 0, 0,  0, 0,  1, 1, 1, 1},
-  {vp[5][1], vp[5][2], vp[5][3],  -1, 0, 0,  0, 1,  1, 1, 1, 1},
-  {vp[8][1], vp[8][2], vp[8][3],  -1, 0, 0,  1, 0,  1, 1, 1, 1},
-
-  -- 6 2 4 8
-  {vp[4][1], vp[4][2], vp[4][3],  0, -1, 0,  0, 1,  1, 1, 1, 1},
-  {vp[2][1], vp[2][2], vp[2][3],  0, -1, 0,  1, 1,  1, 1, 1, 1},
-  {vp[6][1], vp[6][2], vp[6][3],  0, -1, 0,  1, 0,  1, 1, 1, 1}, -- blue OK
-  {vp[8][1], vp[8][2], vp[8][3],  0, -1, 0,  0, 0,  1, 1, 1, 1},
-  {vp[4][1], vp[4][2], vp[4][3],  0, -1, 0,  0, 1,  1, 1, 1, 1},
-  {vp[6][1], vp[6][2], vp[6][3],  0, -1, 0,  1, 0,  1, 1, 1, 1},
-
-  -- 2 1 3 4
-  {vp[3][1], vp[3][2], vp[3][3],  1, 0, 0,  0, 1,  1, 1, 1, 1}, -- white
-  {vp[1][1], vp[1][2], vp[1][3],  1, 0, 0,  1, 1,  1, 1, 1, 1},
-  {vp[2][1], vp[2][2], vp[2][3],  1, 0, 0,  1, 0,  1, 1, 1, 1},
-  {vp[4][1], vp[4][2], vp[4][3],  1, 0, 0,  0, 0,  1, 1, 1, 1},
-  {vp[3][1], vp[3][2], vp[3][3],  1, 0, 0,  0, 1,  1, 1, 1, 1},
-  {vp[2][1], vp[2][2], vp[2][3],  1, 0, 0,  1, 0,  1, 1, 1, 1},
-
-  -- 6 5 1 2
-  {vp[1][1], vp[1][2], vp[1][3],  0, 0, -1,  0, 1,  1, 1, 1, 1},
-  {vp[5][1], vp[5][2], vp[5][3],  0, 0, -1,  1, 1,  1, 1, 1, 1},
-  {vp[6][1], vp[6][2], vp[6][3],  0, 0, -1,  1, 0,  1, 1, 1, 1}, -- yellow OK
-  {vp[2][1], vp[2][2], vp[2][3],  0, 0, -1,  0, 0,  1, 1, 1, 1},
-  {vp[1][1], vp[1][2], vp[1][3],  0, 0, -1,  0, 1,  1, 1, 1, 1},
-  {vp[6][1], vp[6][2], vp[6][3],  0, 0, -1,  1, 0,  1, 1, 1, 1},
-})
+cube:set_vertices(require("cube"))
 cube:finalize(true)
 
 local cubet = {
@@ -95,9 +27,6 @@ local cubet = {
   sz = 1,
   sy = 1,
 }
-
-
-local cubes = ecs.query("model", "trans_mat")
 
 local prng = core.create_prng()
 for _=1, 1000 do
@@ -130,42 +59,3 @@ event.on("@tick", function()
   end
 end)
 
-local r = 0
-local vry = core.create_lerped_num()
-
-event.on("@tick", function()
-  r = r + math.rad(2)
-  vry:set(r * 0.5)
-end)
-
-local view_mat
-local perspective_mat
-
-event.on("@frame", function()
-  local v_pitch = core.mat4_identity()
-  v_pitch:rotate(math.rad(-45), 0, 0)
-  local vt = core.mat4_identity()
-  vt:translate(0, 7, -7)
-  local v_yaw = core.mat4_identity()
-  v_yaw:rotate(0, vry:get(), 0)
-  view_mat = v_pitch:mult(vt:mult(v_yaw))
-
-  local sw, sh = core.get_screen_size()
-  local a = sw / sh
-  perspective_mat = core.mat4_identity()
-  perspective_mat:perspective(45, a, 1, 100)
-end)
-
-event.on("@draw", function()
-  shader:bind()
-  shader:send_mat4("v", view_mat)
-  shader:send_mat4("p", perspective_mat)
-
-  tex:bind(0)
-  shader:sendi("tex0", 0)
-
-  for _, cube_ent in ipairs(cubes.ents) do
-    shader:send_mat4("m", cube_ent.trans_mat)
-    cube_ent.model:draw()
-  end
-end)
---
+++ game/systems/draw_meshes.lua
@@ -0,0 +1,44 @@
+local r = 0
+local vry = core.create_lerped_num()
+
+event.on("@tick", function()
+  r = r + math.rad(2)
+  vry:set(r * 0.5)
+end)
+
+local view_mat
+local perspective_mat
+
+event.on("@frame", function()
+  local v_pitch = core.mat4_identity()
+  v_pitch:rotate(math.rad(-45), 0, 0)
+  local vt = core.mat4_identity()
+  vt:translate(0, 7, -7)
+  local v_yaw = core.mat4_identity()
+  v_yaw:rotate(0, vry:get(), 0)
+  view_mat = v_pitch:mult(vt:mult(v_yaw))
+
+  local sw, sh = core.get_screen_size()
+  local a = sw / sh
+  perspective_mat = core.mat4_identity()
+  perspective_mat:perspective(45, a, 1, 100)
+end)
+
+local cubes = ecs.query("model", "trans_mat")
+local shader = core.load_shader("res/vdefault.glsl", "res/fdefault.glsl")
+local tex = core.load_tex("res/textures/bricks.png")
+tex:set_filter(core.tex_filter.NEAREST, core.tex_filter.NEAREST)
+
+event.on("@draw", function()
+  shader:bind()
+  shader:send_mat4("v", view_mat)
+  shader:send_mat4("p", perspective_mat)
+
+  tex:bind(0)
+  shader:sendi("tex0", 0)
+
+  for _, cube_ent in ipairs(cubes.ents) do
+    shader:send_mat4("m", cube_ent.trans_mat)
+    cube_ent.model:draw()
+  end
+end)
